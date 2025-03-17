027429594ce7c422338c92073c2b2d7dd1d492f5
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-03-17 23:51:22 +0100

    Update page title and integrate Beer component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Beer from "./Beer.svelte";
+</script>
+
+<h1>Wobble Watch</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Beer/>
